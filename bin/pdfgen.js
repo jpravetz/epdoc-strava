3471a090720815023787f8b4b1c1eb500557025b
Jim Pravetz <jpravetz@gmail.com>
2025-08-06 13:17:53 -0600

    Remove unused file

--- bin/pdfgen.js
+++
@@ -1,9 +0,0 @@
-/*************************************************************************
- * Copyright(c) 2012-2014 Jim Pravetz <jpravetz@epdoc.com>
- * May be freely distributed under the MIT license.
- **************************************************************************/
-
-var DocLog = require('../lib/log/docLog');
-
-var docLog = new DocLog();
-docLog.create();
